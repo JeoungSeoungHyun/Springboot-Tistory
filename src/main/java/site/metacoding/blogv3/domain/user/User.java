a949ee8ee6440c1fe838cb9c64e5c620db66733f
tjdgus7562 <tjdgus7562@naver.com>
2022-04-11 16:08:07 +0900

    회원가입 완료

--- src/main/java/site/metacoding/blogv3/domain/user/User.java
+++ src/main/java/site/metacoding/blogv3/domain/user/User.java
@@ -28,11 +28,11 @@ public class User {
     @Id
     private Integer id;
 
-    @Column(length = 12, nullable = false, unique = true)
+    @Column(length = 100, nullable = false, unique = true)
     private String username;
 
     // 그대로 넣으면 안되니까 SHA256(해쉬 알고리즘)을 사용 = > 이렇게 안하면 시큐리티가 거부
-    @Column(length = 20, nullable = false)
+    @Column(length = 100, nullable = false)
     private String password;
 
     @Column(length = 20, nullable = false)
--- src/main/java/site/metacoding/blogv3/web/UserController.java
+++ src/main/java/site/metacoding/blogv3/web/UserController.java
@@ -1,10 +1,26 @@
 package site.metacoding.blogv3.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv3.handler.ex.CustomException;
+import site.metacoding.blogv3.service.UserService;
+import site.metacoding.blogv3.web.dto.user.JoinReqDto;
+
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/login-form")
     public String loginForm() {
@@ -15,4 +31,27 @@ public class UserController {
     public String joinForm() {
         return "user/joinForm";
     }
+
+    @PostMapping("join")
+    public String join(@Valid JoinReqDto joinReqDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                // System.out.println(fe.getField());
+                // System.out.println(fe.getDefaultMessage());
+                errorMap.put(fe.getField(), fe.getDefaultMessage());
+            }
+            // data 리턴인지 html 리턴인지 구분해준다.
+            throw new CustomException(errorMap.toString());
+        }
+
+        // 핵심로직
+
+        userService.회원가입(joinReqDto.toEntity());
+
+        return "redirect:/login-form";
+    }
+
 }
