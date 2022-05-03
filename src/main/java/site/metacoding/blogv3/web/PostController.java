cd8b3c142701d83bc599118c5294a97ba219612f
tjdgus7562 <tjdgus7562@naver.com>
2022-05-03 09:12:27 +0900

    글쓰기등록페이지로 카테고리 목록 가져오기

--- src/main/java/site/metacoding/blogv3/web/PostController.java
+++ src/main/java/site/metacoding/blogv3/web/PostController.java
@@ -1,15 +1,21 @@
 package site.metacoding.blogv3.web;
 
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv3.config.auth.LoginUser;
+import site.metacoding.blogv3.domain.category.Category;
+import site.metacoding.blogv3.handler.ex.CustomException;
 import site.metacoding.blogv3.service.PostService;
 import site.metacoding.blogv3.web.dto.post.PostRespDto;
+import site.metacoding.blogv3.web.dto.post.PostWriteReqDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,6 +23,26 @@ public class PostController {
 
     // 카테고리와 게시글의 정보도 같이 들고 가야하기 때문에 CategorySerivce가 아닌 PostService에서 처리
     private final PostService postService;
+
+    @PostMapping("/s/post")
+    public String write(PostWriteReqDto postWriteReqDto, @AuthenticationPrincipal LoginUser loginUser) {
+
+        postService.게시글쓰기(postWriteReqDto);
+
+        return "redirect:/user/" + loginUser.getUser().getId() + "/post";
+    }
+
+    @GetMapping("/s/post/write-form")
+    public String writeForm(@AuthenticationPrincipal LoginUser loginUser, Model model) {
+        List<Category> categorys = postService.게시글쓰기화면(loginUser.getUser());
+
+        if (categorys.size() == 0) {
+            throw new CustomException("카테고리 등록이 필요해요");
+        }
+
+        model.addAttribute("categorys", categorys);
+        return "/post/writeForm";
+    }
 
     @GetMapping("/user/{id}/post")
     public String postList(@PathVariable Integer id, @AuthenticationPrincipal LoginUser loginUser, Model model) {
